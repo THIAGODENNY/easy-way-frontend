ff5cf58848d982cdfeb0e3b68cc223065bd50e06
Marcos Marossi <marcos_marossi@hst.com.br>
2020-06-29 14:06:07 -0300

    Change font size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Form, Button } from 'react-bootstrap';
 const Header = ({ handleRoute, handleLogout }) => (
   <Navbar bg="light" variant="light">
     <Navbar.Brand onClick={() => handleRoute('agendamentos')}>
-      <img src={require('../../assets/logo.png')} style={{height: '30px'}} alt="logo"/>     EasyWay
+      <img src={require('../../assets/logo.png')} style={{height: '30px'}} alt="logo"/>       EasyWay
     </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link onClick={() => handleRoute('agendamentos')}>{}Agendamentos{}</Nav.Link>
